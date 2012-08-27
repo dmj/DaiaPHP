e50ec4b6cd89e7531ffdf7e18cb279e3cbd9beb7
David Maus <maus@hab.de>
2012-08-27 06:28:12 +0200

    Implement basic conversion flow.
    * php/HAB/Daia/Converter/PicaRecordConverter.php (serviceOpenAccess)
    (serviceInterloan, serviceLoan, servicePresentation): New
    functions. Service constructors.
    (prune): New function. Return final set of DAIA items.
    (getServiceConstructorMap): New function. Return map of service
    constructors indexed by general availability indicator.
    (getServiceConstructors): New function. Return service constructors by
    general availability indicator.
    (addAvailability): New function. Add availability information to item
    collection.
    (createItems): New function. Return items for a copy record.
    (createItemCollection): New function. Create item collection for local
    record.
    (init): New function. Initialize converter.
    (convert): Implement conversion flow.
    ($localRecord): New variable. Currently processed local record.
    ($itemCollection): New variable. Current item collection.

--- src/php/HAB/Daia/Converter/PicaRecordConverter.php
+++ src/php/HAB/Daia/Converter/PicaRecordConverter.php
@@ -37,6 +37,20 @@ class PicaRecordConverter
 {
 
     /**
+     * Collection of items indexed by copy record.
+     *
+     * @var \SplObjectStorage
+     */
+    protected $itemCollection;
+
+    /**
+     * Currently processed local record.
+     *
+     * @var \HAB\Pica\Record\LocalRecord
+     */
+    protected $localRecord;
+
+    /**
      * Constructor.
      *
      * @return void
@@ -53,6 +67,162 @@ class PicaRecordConverter
      */
     public function convert (\HAB\Pica\Record\LocalRecord $localRecord)
     {
+        $this->init($localRecord);
+        $this->createItemCollection();
+        $this->addAvailability();
+        return $this->prune();
+    }
+
+    /**
+     * Initialize converter for a new converting process.
+     *
+     * @param  \HAB\Pica\Record\LocalRecord $localRecord Local record to convert
+     * @return void
+     */
+    protected function init (\HAB\Pica\Record\LocalRecord $localRecord)
+    {
+        $this->itemCollection = new \SplObjectStorage();
+        $this->localRecord    = $localRecord;
+    }
+
+    /**
+     * Create initial DAIA item collection.
+     *
+     * @return void
+     */
+    protected function createItemCollection ()
+    {
+        foreach ($this->localRecord->getCopyRecords() as $copyRecord) {
+            $items = $this->createItems($copyRecord);
+            if (!empty($items)) {
+                $this->itemCollection->attach($copyRecord, $items);
+            }
+        }
+    }
+
+    /**
+     * Return array of items for a copy record.
+     *
+     * @param  \HAB\Pica\Record\CopyRecord $copyRecord
+     * @return array
+     */
+    protected function createItems (\HAB\Pica\Record\CopyRecord $copyRecord)
+    {
+        $item = new \HAB\Daia\Item();
+        $sigField = $copyRecord->getFirstMatchingField('209A');
+        if ($sigField) {
+            list($sig, $loc) = $sigField->getSubfields('a', 'f');
+            if ($sig) {
+                $item->setLabel(
+                    new \HAB\Daia\Label($sig->getValue())
+                );
+            }
+            if ($loc) {
+                $storage = new \HAB\Daia\Storage();
+                $storage->setContent($loc->getValue());
+            }
+        }
+        $epnField = $copyRecord->getFirstMatchingField('201@');
+        if ($epnField) {
+            list($epn) = $epnField->getSubfields('e');
+            if ($epn) {
+                $item->setId($epn->getValue());
+            }
+        }
+        return array($item);
+    }
+
+    /**
+     * Add availability information to DAIA item collection.
+     *
+     * @return void
+     */
+    protected function addAvailability ()
+    {
+        foreach ($this->itemCollection as $copyRecord) {
+            // Get general availability indicator
+            $indField = $copyRecord->getFirstMatchingField('209A');
+            if ($indField) {
+                list($ind) = $indField->getSubfields('d');
+                if ($ind) {
+                    $items = $this->itemCollection->offsetGet($copyRecord);
+                    foreach ($this->getServiceConstructors($ind->getValue()) as $serviceConstructor) {
+                        $availability = call_user_func($serviceConstructor, $copyRecord);
+                        if ($availability) {
+                            \HAB\Helper::mapMethod($items, 'addAvailability', array($availability));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Return service constructors for general availability indicator.
+     *
+     * @param  string $ind General availability indicator
+     * @return array
+     */
+    protected function getServiceConstructors ($ind)
+    {
+        $map = $this->getServiceConstructorMap();
+        return isset($map[$ind]) ? $map[$ind] : array();
+    }
+
+    /**
+     * Return service constructor map.
+     *
+     * @return array
+     */
+    protected function getServiceConstructorMap ()
+    {
+        return array(
+            'u' => array(
+                array($this, 'servicePresentation'),
+                array($this, 'serviceLoan'),
+                array($this, 'serviceInterloan'),
+            )
+        );
+    }
+
+    /**
+     * Return final set of DAIA items.
+     *
+     * @return array
+     */
+    protected function prune ()
+    {
+        $items = array();
+        foreach ($this->itemCollection as $copyRecord) {
+            $recordItems = $this->itemCollection->offsetGet($copyRecord);
+            if (!empty($recordItems)) {
+                $items = array_merge($items, $recordItems);
+            }
+        }
+        return $items;
+    }
+
+    /// Service constructors
+
+    protected function servicePresentation (\HAB\Pica\Record\CopyRecord $copyRecord)
+    {
+        return new \HAB\Daia\Available('presentation');
+    }
+
+    protected function serviceLoan (\HAB\Pica\Record\CopyRecord $copyRecord)
+    {
+        $avail = new \HAB\Daia\Available('loan');
+        return $avail;
+    }
+
+    protected function serviceInterloan (\HAB\Pica\Record\CopyRecord $copyRecord)
+    {
+        return new \HAB\Daia\Available('interloan');
+    }
+
+    protected function serviceOpenAccess (\HAB\Pica\Record\CopyRecord $copyRecord)
+    {
+        return new \HAB\Daia\Available('openaccess');
     }
 
 }
